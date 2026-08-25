0eee777e3df2ecb95f3ebefc97800e3e772f1f63
hyunwoo <htelro02@gmail.com>
2026-08-25 16:58:37 +0900

    Bust cached atlas modules

--- math-test/assets/concept-browser.js
+++ math-test/assets/concept-browser.js
@@ -1,7 +1,7 @@
 const script = document.currentScript;
 Promise.all([
-  import('./concepts/index.js'),
-  import('./concept-catalog.js')
+  import('./concepts/index.js?v=20260825-12'),
+  import('./concept-catalog.js?v=20260825-12')
 ]).then(([{ concepts, conceptsById }, { areas, commonQuestions }]) => {
   const host = document.querySelector('[data-concept-browser]');
   if (!host) return;
