bed2ec1e490d10fc945a1911093fd43b2056959c
hyunwoo <htelro02@gmail.com>
2026-08-26 16:29:50 +0900

    Deepen shared analysis algebra and counting concepts

--- math-test/assets/concept-browser.js
+++ math-test/assets/concept-browser.js
@@ -1,7 +1,7 @@
 const script = document.currentScript;
 Promise.all([
-  import('./concepts/index.js?v=20260826-33'),
-  import('./concept-catalog.js?v=20260826-33')
+  import('./concepts/index.js?v=20260826-34'),
+  import('./concept-catalog.js?v=20260826-34')
 ]).then(([{ concepts, conceptsById }, { areas, commonQuestions }]) => {
   const host = document.querySelector('[data-concept-browser]');
   if (!host) return;
