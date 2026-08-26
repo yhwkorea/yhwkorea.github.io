3408e7676435c8e34f53ecba442a4e6c2261d199
hyunwoo <htelro02@gmail.com>
2026-08-26 16:53:45 +0900

    Deepen core probability concepts

--- math-test/assets/concept-browser.js
+++ math-test/assets/concept-browser.js
@@ -1,7 +1,7 @@
 const script = document.currentScript;
 Promise.all([
-  import('./concepts/index.js?v=20260826-35'),
-  import('./concept-catalog.js?v=20260826-35')
+  import('./concepts/index.js?v=20260826-36'),
+  import('./concept-catalog.js?v=20260826-36')
 ]).then(([{ concepts, conceptsById }, { areas, commonQuestions }]) => {
   const host = document.querySelector('[data-concept-browser]');
   if (!host) return;
