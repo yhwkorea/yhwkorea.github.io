373d02d4bb53f8b7d83e222b6658c48134b48b51
hyunwoo <htelro02@gmail.com>
2026-08-26 17:04:21 +0900

    Deepen foundational number theory concepts

--- math-test/assets/concept-browser.js
+++ math-test/assets/concept-browser.js
@@ -1,7 +1,7 @@
 const script = document.currentScript;
 Promise.all([
-  import('./concepts/index.js?v=20260826-37'),
-  import('./concept-catalog.js?v=20260826-37')
+  import('./concepts/index.js?v=20260826-38'),
+  import('./concept-catalog.js?v=20260826-38')
 ]).then(([{ concepts, conceptsById }, { areas, commonQuestions }]) => {
   const host = document.querySelector('[data-concept-browser]');
   if (!host) return;
