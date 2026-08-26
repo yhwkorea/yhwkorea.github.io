76b87ed32b6425bd3092634da0db84086dd17c41
hyunwoo <htelro02@gmail.com>
2026-08-26 17:23:52 +0900

    Deepen foundational differential equations concepts

--- math-test/assets/concept-browser.js
+++ math-test/assets/concept-browser.js
@@ -1,7 +1,7 @@
 const script = document.currentScript;
 Promise.all([
-  import('./concepts/index.js?v=20260826-39'),
-  import('./concept-catalog.js?v=20260826-39')
+  import('./concepts/index.js?v=20260826-40'),
+  import('./concept-catalog.js?v=20260826-40')
 ]).then(([{ concepts, conceptsById }, { areas, commonQuestions }]) => {
   const host = document.querySelector('[data-concept-browser]');
   if (!host) return;
--- math-test/assets/journey-ui.js
+++ math-test/assets/journey-ui.js
@@ -1,4 +1,4 @@
-import { cryptoBranches, schoolBranches, mathBranches, journeys, areaForPath } from './site-map.js?v=20260826-39';
+import { cryptoBranches, schoolBranches, mathBranches, journeys, areaForPath } from './site-map.js?v=20260826-40';
 import { getJourney, startJourney, descend, reconcile, returnTo, endJourney, subscribe } from './journey-store.js';
 
 if (!window.PQCJourney) {
