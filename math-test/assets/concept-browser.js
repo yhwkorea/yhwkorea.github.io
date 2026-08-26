b01bff87b890d869abd6e9ed11e540b4ed201893
hyunwoo <htelro02@gmail.com>
2026-08-26 10:21:20 +0900

    Expand probability distributions and conditional structure

--- math-test/assets/concept-browser.js
+++ math-test/assets/concept-browser.js
@@ -1,7 +1,7 @@
 const script = document.currentScript;
 Promise.all([
-  import('./concepts/index.js?v=20260826-17'),
-  import('./concept-catalog.js?v=20260826-17')
+  import('./concepts/index.js?v=20260826-18'),
+  import('./concept-catalog.js?v=20260826-18')
 ]).then(([{ concepts, conceptsById }, { areas, commonQuestions }]) => {
   const host = document.querySelector('[data-concept-browser]');
   if (!host) return;
