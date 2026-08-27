c7b4eff17874bb8c46f6ab5a1abe3caa1a294808
hyunwoo <htelro02@gmail.com>
2026-08-27 13:38:08 +0900

    Deepen core mathematical statistics concepts

--- math-test/assets/concept-browser.js
+++ math-test/assets/concept-browser.js
@@ -1,7 +1,7 @@
 const script = document.currentScript;
 Promise.all([
-  import('./concepts/index.js?v=20260827-49'),
-  import('./concept-catalog.js?v=20260827-49')
+  import('./concepts/index.js?v=20260827-50'),
+  import('./concept-catalog.js?v=20260827-50')
 ]).then(([{ concepts, conceptsById }, { areas, commonQuestions }]) => {
   const host = document.querySelector('[data-concept-browser]');
   if (!host) return;
