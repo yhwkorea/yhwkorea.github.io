7aa82940496cebcb9a58280a8a17b2aa0bee39f8
hyunwoo <htelro02@gmail.com>
2026-08-27 10:46:34 +0900

    Deepen core numerical analysis concepts

--- math-test/assets/concept-browser.js
+++ math-test/assets/concept-browser.js
@@ -1,7 +1,7 @@
 const script = document.currentScript;
 Promise.all([
-  import('./concepts/index.js?v=20260827-47'),
-  import('./concept-catalog.js?v=20260827-47')
+  import('./concepts/index.js?v=20260827-48'),
+  import('./concept-catalog.js?v=20260827-48')
 ]).then(([{ concepts, conceptsById }, { areas, commonQuestions }]) => {
   const host = document.querySelector('[data-concept-browser]');
   if (!host) return;
