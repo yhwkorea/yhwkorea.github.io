8b2dd6e69d1f9467c6ab1bc099b0fcfa18d678c1
hyunwoo <htelro02@gmail.com>
2026-08-27 13:21:23 +0900

    Deepen core optimization concepts

--- math-test/assets/journey-ui.js
+++ math-test/assets/journey-ui.js
@@ -1,4 +1,4 @@
-import { cryptoBranches, schoolBranches, mathBranches, journeys, areaForPath } from './site-map.js?v=20260827-48';
+import { cryptoBranches, schoolBranches, mathBranches, journeys, areaForPath } from './site-map.js?v=20260827-49';
 import { getJourney, startJourney, descend, reconcile, returnTo, endJourney, subscribe } from './journey-store.js';
 
 if (!window.PQCJourney) {
